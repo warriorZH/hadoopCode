7b672d2f38301e2da19a88e840ffaf7c6cfb6997
warriorZH <zhangpeihua123@gmail.com>
2016-04-15 22:29:43 +0800

    add unique opt test in java for hadoop

---
+++ javaCode/Unique/src/Unique.java
@@ -0,0 +1,49 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * Created by warrior on 16-4-15.
+ */
+public class Unique {
+
+    public static class UniMapper
+        extends Mapper<Object, Text, Text, Text>{
+        private static Text line = new Text();
+        public void map(Object key, Text value, Context context)
+            throws IOException, InterruptedException{
+            line = value;
+            context.write(line,new Text(""));
+        }
+    }
+
+    public static class UniReduce
+        extends Reducer<Text, Text, Text, Text>{
+        public void reduce(Text key, Iterable<Text> values, Context context)
+            throws IOException, InterruptedException{
+            context.write(key, new Text(""));
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Unique operate");
+        job.setJarByClass(Unique.class);
+        job.setMapperClass(UniMapper.class);
+        job.setCombinerClass(UniReduce.class);
+        job.setReducerClass(UniReduce.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true)? 0:1);
+    }
+}
